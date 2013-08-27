bda2cb8c6bbfc98d56d7ae8b7037449d3f582aa6
Gabe Marshall <gabemarshall@me.com>
2013-08-27 11:02:29 -0500

    v0.0.1

---
+++ xss.js
@@ -0,0 +1,91 @@
+var casper = require('casper').create({
+	logLevel: 'warning',
+	verbose: true,
+	onAlert: function(msg){
+		tempMessage = 'XSS verified '+msg+'';
+		casper.log('XSS verified '+msg, 'warning');
+		vulns.push(tempMessage);
+
+	}
+});
+
+var fs = require('fs');
+var utils = require('utils');
+var data = fs.read('rsnake.txt');
+var xss = data.toString().split("\n");
+var payloads = [];
+
+var params = casper.cli.get("param");
+var url = casper.cli.get("url");
+var vulns = [];
+var count;
+
+if (!params){
+	console.log('Welcome to CasperXSS\nExample usage: casperjs xss.js --url=example.com --param=example ')
+	casper.exit();
+}
+
+if(params && !url){
+	console.log('URL is missing, please try again Ex: casperjs xss.js --url=example.com --param=example')
+	casper.exit();
+}
+
+console.log('\nTrying '+xss.length+' payloads on the \''+params+'\' parameter. \nSit back and enjoy the ride.\n');
+
+// add regex to clean up xss validation msg (\[object Casper\], currently at)
+
+
+	for (z = 0; z < xss.length; z++) {
+		payloads.push(url+'?' + params + '=' + xss[z] + '');
+	}
+
+	casper.start(url, function(status) {
+
+
+	});
+	casper.run()
+
+
+
+casper.then(function() {
+    // temporarily registering listener
+    this.on('remote.alert', function(){
+    	this.capture('ugh.png');
+    });
+});
+
+function test(url, count, total) {
+
+	casper.thenOpen(url, function(status) {
+
+		//Page is loaded!
+		console.log('Current Payload: '+url);
+
+		if(count === total -1){
+			
+			console.log('Scan Completed!');
+			console.log(vulns.length+' payloads succeeded');
+
+			if(vulns){
+			console.log(vulns[0])
+			}
+			
+		}
+
+	});
+
+}
+
+function test1(){
+	console.log('finished');
+}
+
+function finish(){
+	console.log('Finished!');
+}
+
+for (i = 0; i < payloads.length; i++) {
+	
+		test(payloads[i], i, payloads.length);
+}
+
